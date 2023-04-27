1b4d7ab4e8a9e69acb118202620fb3773299862a
Mark Paul Cañeda <markpaulcaneda1997@gmail.com>
2023-04-27 08:47:28 +0800

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const rapidApiKey = '8d47e06fdcmshebc962f44221b6bp1be45fjsn9f3a2bcddefa';
-export const rapidApiHost = 'openai80.p.rapidapi.com';
-export const rapidApiChatCompletions = 'https://openai80.p.rapidapi.com/chat/completions';
